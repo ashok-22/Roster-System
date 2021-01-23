c38921201fc949a69abe8ef00b3af03598e1753a
Ashok Mogathala <ashok-22@github.com>
2021-01-23 17:31:08 +0530

    Enter

--- client/src/401.js
+++ client/src/401.js
@@ -8,7 +8,7 @@ function Unauthorized(){
         <div className="text-center" style={{"margin-top":"100px"}}>
             <img src={errorimg} alt="401 error" style={{"height":"250px","width":"300px","border-radius":"5px"}}></img>
             <h2>HTTP Error 401:Un authorized access</h2>
-            <p>You have attempted to access a page which you are not authorized. Please go back to <Link to={'/login'}><span className="h4">LOGIN PAGE</span></Link> and login again</p>
+            <p>You have attempted to access a page which you are not authorized to. Please go back to <Link to={'/login'}><span className="h4">LOGIN PAGE</span></Link> and login again</p>
         </div>
     )
 }
